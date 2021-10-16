c1831bb0775f4466dbe073cd489ad1a176c87ef2
Maks <maksym.klovak@gmail.com>
2021-10-16 18:20:35 +0300

    Update app.py

--- services/replicated_log_secondary_1/app.py
+++ services/replicated_log_secondary_1/app.py
@@ -1,57 +1,79 @@
-import logging
-import threading
-from concurrent import futures
-
+import asyncio
 import grpc
-from flask import Flask
-from flask_restful import reqparse, Api, Resource
+import time
 
-from proto import replicated_log_pb2 as pb2, replicated_log_pb2_grpc as pb2_grpc
+from aiohttp import web
+from grpc.experimental.aio import init_grpc_aio
 
-""" HTTP server (Flask) with REST API endpoint """
-app = Flask(__name__)
-api = Api(app)
-parser = reqparse.RequestParser()
-
-
-class MessageList(Resource):
-    def get(self):
-        print(f"Current messages on this server: {MESSAGES}")
-        return MESSAGES
-
-
-api.add_resource(MessageList, '/messages')
-
-
-def http_app():
-    print("====== Start HTTP server  ======")
-    app.run()
-
-
-class ReplicatedLogServicer(pb2_grpc.ReplicatedLogServicer):
-    """ gRCP server """
-
-    def ReplicateMessage(self, request, context):
-        print(f"got message from Primary: {request.message}")
-        MESSAGES.append(request.message)
-        return pb2.ReplicateMessageResponse(response="OK")
-
-
-def rpc_app():
-    logging.basicConfig()
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    pb2_grpc.add_ReplicatedLogServicer_to_server(ReplicatedLogServicer(), server)
-    server.add_insecure_port('0.0.0.0:50051')
-    print("====== Start gRPC server ======")
-    server.start()
-    server.wait_for_termination()
-
+from proto.replicated_log_pb2 import ReplicateMessageResponse
+from proto.replicated_log_pb2_grpc import add_ReplicatedLogServicer_to_server
+from proto.replicated_log_pb2_grpc import ReplicatedLogServicer
 
 """ List to store messages """
 MESSAGES = []
 
+class MessagesView(web.View):
+    async def get(self):
+        return web.Response(text=str(MESSAGES))
+
+class Application(web.Application):
+    def __init__(self):
+        super().__init__()
+        self.grpc_task = None
+        self.grpc_server = GrpcServer()
+        self.add_routes()
+        self.on_startup.append(self.__on_startup())
+        self.on_shutdown.append(self.__on_shutdown())
+
+    def __on_startup(self):
+        async def _on_startup(app):
+            self.grpc_task = \
+                asyncio.ensure_future(app.grpc_server.start())
+
+        return _on_startup
+
+    def __on_shutdown(self):
+        async def _on_shutdown(app):
+            await app.grpc_server.stop()
+            app.grpc_task.cancel()
+            await app.grpc_task
+
+        return _on_shutdown
+
+    def add_routes(self):
+        self.router.add_view('/messages', MessagesView)
+
+    def run(self):
+        return web.run_app(self, port=5000)
+
+
+class LogServicer(ReplicatedLogServicer):
+    def ReplicateMessage(self, request, context):
+        print(f"got message from Primary: {request.message}")
+        MESSAGES.append(request.message)
+        # time.sleep(5)  # Delays for 5 seconds.
+        return ReplicateMessageResponse(response=f"OK, {MESSAGES}")
+
+
+class GrpcServer:
+    def __init__(self):
+        init_grpc_aio()
+        self.server = grpc.experimental.aio.server()
+        self.servicer = LogServicer()
+        add_ReplicatedLogServicer_to_server(
+            self.servicer,
+            self.server)
+        self.server.add_insecure_port("[::]:50051")
+
+    async def start(self):
+        await self.server.start()
+        await self.server.wait_for_termination()
+
+    async def stop(self):
+        await self.servicer.close()
+        await self.server.wait_for_termination()
+
+
+application = Application()
 if __name__ == '__main__':
-    t1 = threading.Thread(target=http_app)
-    t2 = threading.Thread(target=rpc_app)
-    t1.start()
-    t2.start()
+    application.run()
