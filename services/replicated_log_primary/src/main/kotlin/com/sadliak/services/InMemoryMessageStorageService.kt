a129867f43da299241c7e8a231d2852e5258d3bc
Oleksii Sadliak <o.sadliak@gmail.com>
2021-11-24 19:53:50 +0200

    Use CopyOnWriteArrayList for storing messages in replicated_log_primary service

--- services/replicated_log_primary/src/main/kotlin/com/sadliak/services/InMemoryMessageStorageService.kt
+++ services/replicated_log_primary/src/main/kotlin/com/sadliak/services/InMemoryMessageStorageService.kt
@@ -1,24 +1,19 @@
 package com.sadliak.services
 
 import com.sadliak.models.Message
-import java.util.*
+import java.util.concurrent.CopyOnWriteArrayList
 import javax.enterprise.context.ApplicationScoped
 
 
 @ApplicationScoped
 class InMemoryMessageStorageService : MessageStorageService {
-
-    private val messages: MutableList<Message> = Collections.synchronizedList(mutableListOf());
+    private val messages: MutableList<Message> = CopyOnWriteArrayList();
 
     override fun addMessage(message: Message) {
-        synchronized(this) {
-            this.messages.add(message)
-        }
+        this.messages.add(message)
     }
 
     override fun listMessages(): List<Message> {
-        synchronized(this) {
-            return this.messages
-        }
+        return this.messages
     }
 }
